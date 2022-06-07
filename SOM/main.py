d8842cf33387c41824d6f6fd547cdb8d1f038a2d
AmitSheer <amitcohen95@gmail.com>
2022-06-07 13:54:46 +0300

    fixed learning

--- SOM/main.py
+++ SOM/main.py
@@ -1,26 +1,87 @@
 import random
 import numpy as np
+from matplotlib import pyplot as plt
 
 from som import SOM
 
 DATA_SIZE = 1000
 
 
-def generateData(part, data_size=DATA_SIZE, debug=False, ):
+def generate_unbalanced(data_size=DATA_SIZE, a=True):
+    np.random.seed(1)
+    if a:
+        x_1 = np.random.uniform(0.5, 1, int(data_size / 4 * 3))
+        x_2 = np.random.uniform(0, 0.5, int(data_size / 4 * 1))
+        y = np.random.uniform(0, 1, data_size)
+        x = np.concatenate((x_1, x_2), dtype=float)
+    else:
+        x_1 = np.random.uniform(0.5, 1, int(data_size / 2))
+        x_2 = np.random.uniform(0, 0.5, int(data_size / 2))
+        y_1 = np.random.uniform(0.5, 1, int(data_size / 2))
+        y_2 = np.random.uniform(0, 0.5, int(data_size / 2))
+        x = np.concatenate((x_1, x_2), dtype=float)
+        y = np.concatenate((y_1, y_2), dtype=float)
+    return np.array([x, y]).T
+
+
+def generateData(part, data_size=DATA_SIZE, debug=False):
     np.random.seed(1)
     if part == 1:
-        return np.random.rand(data_size, 2)#.astype(np.float64)
-    return np.zeros(data_size, 2)#.astype(np.float64)
+        return np.random.rand(data_size, 2)  # .astype(np.float64)
+    return np.zeros(data_size, 2)  # .astype(np.float64)
+
+
+def generate_donut(data_size=DATA_SIZE):
+    # np.random.seed(1)
+    donut = []
+
+    while len(donut) != data_size:
+        p = np.random.uniform(-2, 2, 2)
+        if 4 >= np.square(p).sum() >= 2:
+            donut.append(p)
+    return np.array(donut)
+
+
+def generateDisk(data_size=DATA_SIZE):
+    np.random.seed(1)
+    radius = np.random.uniform(0, 1, data_size)
+    theta = np.random.uniform(0, 2 * np.pi, data_size)
+    x = np.sqrt(radius) * np.cos(theta)
+    y = np.sqrt(radius) * np.sin(theta) * 0.5
+    return np.array([x, y]).T
 
 
 def PartA():
-    data = generateData(1)
+    # Q1.a
+    data = generateDisk()
+    model = SOM(rows=1, cols=100, radius=2, learning_rate=0.9)
+    model.train(data, debug=False, epochs=10,
+                fig_path='E:\\Git\\Neuro-Computation\\SOM\\partA\\disk_images\\100_neurons_in_line')
 
-    model = SOM(rows=100, cols=2, radius=2, learning_rate=0.3)
-    model.train(data, debug=True)
-# def train(self, input, epochs=50, times=100, neurons=100, radius=.1, debug=False):
+    # Q1.b
+    model = SOM(rows=10, cols=10, radius=2, learning_rate=0.9)
+    model.train(data, debug=False, epochs=10,
+                fig_path='E:\\Git\\Neuro-Computation\\SOM\\partA\\disk_images\\10x10_neurons')
+
+    # Q1.c
+    data = generate_unbalanced()
+    model = SOM(rows=10, cols=10, radius=2, learning_rate=0.9)
+    model.train(data, debug=False, epochs=10,
+                fig_path='E:\\Git\\Neuro-Computation\\SOM\\partA\\disk_images\\unbalanced\\a')
+    data = generate_unbalanced(a=False)
+    model = SOM(rows=10, cols=10, radius=2, learning_rate=0.4)
+    model.train(data, debug=False, epochs=40,
+                fig_path='E:\\Git\\Neuro-Computation\\SOM\\partA\\disk_images\\unbalanced\\b')
+
+    # Q1.d
+    data = generate_donut()
+    model = SOM(rows=1, cols=30, radius=2, learning_rate=0.4)
+    model.train(data, debug=False, epochs=10,
+                fig_path='E:\\Git\\Neuro-Computation\\SOM\\partA\\disk_images\\donut')
+
+def PartB():
 
 
+    
 if __name__ == '__main__':
-
     PartA()
--- SOM/som.py
+++ SOM/som.py
@@ -10,19 +10,19 @@ class SOM(object):
         self.initial_radius = radius
         self.data = None
         self.epochs = None
-        self.neurons = np.array(np.random.rand(rows, cols))  # , dtype=object)
+        # self.neurons = np.array(np.random.rand(rows, cols))  # , dtype=object)
+        self.neurons = np.random.uniform(0, 1, (rows, cols, 2))
 
     def find_BMU(self, vector):
         min_dis_vector = np.inf
         # bmu = None
-        
 
-        for index, neuron in enumerate(self.neurons):
-            dist = np.linalg.norm(vector - neuron)
-
-            if min_dis_vector > dist:
-                min_dis_vector = dist
-                bmu = index
+        for i, neuron in enumerate(self.neurons):
+            for j, point in enumerate(neuron):
+                dist = np.linalg.norm(vector - point)
+                if min_dis_vector > dist:
+                    min_dis_vector = dist
+                    bmu = [i, j]
                 # ret_index = index
         # for x in range(self.neurons.shape[0]):
         #     for y in range(self.neurons.shape[1]):
@@ -37,36 +37,58 @@ class SOM(object):
 
     def update_neurons(self, bmu, sample_i, iter):
         curr_lr = self.initial_learning_rate * np.exp(-iter / 300)
-        for index, neuron in enumerate(self.neurons):
-            dis = np.linalg.norm(bmu - index)
-            radius = np.exp(-np.power(dis, 2) / self.initial_radius)
-            self.neurons[index] += curr_lr * radius * (sample_i - self.neurons[index])
+        for i, neuron in enumerate(self.neurons):
+            for j, point in enumerate(neuron):
+                dis = np.linalg.norm(np.subtract(bmu, [i, j]))
+                radius = np.exp(-np.power(dis, 2) / self.initial_radius)
+                self.neurons[i, j] += curr_lr * radius * (sample_i - self.neurons[i, j])
 
-    def train(self, input, epochs=5, times=100, neurons=100, radius=.1, debug=False):
+    def train(self, input, epochs=5, times=100, neurons=100, radius=.1, debug=False, fig_path=None):
         self.data = input
         self.epochs = epochs
-
+        self.plot(0, None)
+        iter = 0
         for epoch in np.arange(epochs):
             np.random.shuffle(self.data)
 
             for index, sample in enumerate(self.data):
                 bmu = self.find_BMU(sample)
-
-                if debug and index % 200 == 0:
-                    self.plot(index)  # , epoch)
+                if debug and (index * (epoch + 1)) % 200 == 0:
+                    self.plot(index, None)  # , epoch)
 
                 self.update_neurons(bmu, sample, index)
 
-            if debug:
+                iter += 1
+                if fig_path is not None and iter % 500 == 0:
+                    self.plot_snake(iter, fig_path)
+            if True:
                 print(str(int(epoch / times * 100)) + '%')  # Progress percentage
         self.plot(epochs, None)
 
+    def plot_snake(self, iter, fig_path):
+        neurons = self.neurons.reshape((self.shape[0] * self.shape[1], 2,))
+        fig, ax = plt.subplots()
+        for i in range(self.shape[0]):
+            ax.plot(self.neurons[i, :, 0], self.neurons[i, :, 1], c='r')
+        for i in range(self.shape[1]):
+            ax.plot(self.neurons[:, i, 0], self.neurons[:, i, 1], c='r')
+        ax.scatter(neurons[:, 0], neurons[:, 1], c='r')
+        ax.scatter(self.data[:, 0], self.data[:, 1], alpha=0.8)
+        ax.set_title("Data size: " + str(len(self.data)) + " | "
+                     + "Iteration: " + str(iter) + " | "
+                     # + "Epoch :" + str(epoch) +
+                                                   "lr: " + str(self.initial_learning_rate) + " | "
+                     + "Radius:" + str(self.initial_radius))
+        plt.savefig(
+            f'{fig_path}\\{ax.get_title().replace(" ", "_").replace(":", "").replace("|", "")}.png')
+        plt.show()
+
     def plot(self, iter, sample):  # , epoch):
 
-        x_axis, y_axis = np.hsplit(self.neurons, 2)
+        x_axis, y_axis = self.neurons[:, :, 0], self.neurons[:, :, 1]
 
-        x_axis = x_axis.reshape((self.neurons.shape[0],))
-        y_axis = y_axis.reshape((self.neurons.shape[0],))
+        x_axis = x_axis.reshape((self.shape[0] * self.shape[1],))
+        y_axis = y_axis.reshape((self.shape[0] * self.shape[1],))
 
         fig, ax = plt.subplots()
         ax.scatter(x_axis, y_axis)
